8d205ef5bee0c303e27847afbfb9f309c2cb4358
mrinal mitra <mmitra@aligntech.com>
2017-11-21 02:07:21 +0530

    add page

--- routes/adminPages.js
+++ routes/adminPages.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+/** Get Pages Index **/
 router.get('/',function(req,res){
     res.send('admin area');
+});
+
+/** GET add Page **/
+router.get('/add-page',function(req,res){
+    var title = "";
+    var slug = "";
+    var content = "";
+
+    res.render('admin/add_page',{
+       title: title,
+       slug:slug,
+       content:content
+    })
 });
 
 //Exports
